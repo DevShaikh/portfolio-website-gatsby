bb696807623e5964124b7ff0679732295cb3a2d6
DevShaikh <asofficial4k@gmail.com>
2021-10-11 09:42:35 +0500

    ready to deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,27 +9,29 @@ import { Link } from "gatsby"
 
 export default function Home() {
   return (
-    <Flex alignItems="center" height="81vh" justifyContent="space-between">
-      <Box width="45%">
-        <Box mb="4">
-          <Heading size="3xl" fontWeight="bold">
-            Design
-          </Heading>
-          <Heading size="2xl" fontWeight="light" mt="2" mb="4">
-            Develop & Deploy
-          </Heading>
-          <Text>Jr. Fullstack web developer based in Pakistan.</Text>
+    <Box height="81vh">
+      <Flex alignItems="center" justifyContent="space-between">
+        <Box width="45%">
+          <Box mb="4">
+            <Heading size="3xl" fontWeight="bold">
+              Design
+            </Heading>
+            <Heading size="2xl" fontWeight="light" mt="2" mb="4">
+              Develop & Deploy
+            </Heading>
+            <Text>Jr. Fullstack web developer based in Pakistan.</Text>
+          </Box>
+          <Link to="/projects">
+            <Button variant="primary">My Portfolio & Projects</Button>
+          </Link>
         </Box>
-        <Link to="/projects">
-          <Button variant="primary">My Portfolio & Projects</Button>
-        </Link>
-      </Box>
-      <Box width="55%">
-        <Image
-          src="/svg/portfolio.svg"
-          filter="drop-shadow(2px 4px 6px black) drop-shadow(2px 4px 4px #00000075)"
-        />
-      </Box>
-    </Flex>
+        <Box width="55%">
+          <Image
+            src="/svg/portfolio.svg"
+            filter="drop-shadow(2px 4px 6px black) drop-shadow(2px 4px 4px #00000075)"
+          />
+        </Box>
+      </Flex>
+    </Box>
   )
 }
